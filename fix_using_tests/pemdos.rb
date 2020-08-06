66fe9eba59ec22223e211920de1217d5b60b5d12
Davis Brown <cooliomoded@gmail.com>
2020-08-06 16:42:22 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,10 @@
-# don't forget to add: require 'pry'
+require 'pry'# don't forget to add: require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
     10 * "s" + string
+    puts "Hello"
+    binding.pry
   else
     string
   end
